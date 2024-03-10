e011c0f332ef7bdb6b3a711a28e234b8875a8dc6
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2024-03-10 17:15:42 +0530

    chore: init

---
+++ dist/assets/index-D1p4rnuw.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./_plugin-vue_export-helper-DlAUqK2U.js";import{a as t,o as s,g as o}from"./index-63hZFRlF.js";const c={},a={class:"flex items-center justify-center"},r=o("h1",{class:"text-6xl"},"Dashboard Page",-1),n=[r];function _(i,d){return s(),t("div",a,n)}const m=e(c,[["render",_]]);export{m as default};
